1eae772b8a4c209a83a617c002c6e4bc72a7e99a
Rene Rengel <Rene.Rengel@jala.university>
2023-10-18 10:20:20 -0400

    refacrtoApi

---
+++ src/test/java/basicApi/JsonComparisonTest.java
@@ -0,0 +1,4 @@
+package basicApi;
+
+public class JsonComparisonTest {
+}
---
+++ src/test/java/config/Configuration.java
@@ -0,0 +1,10 @@
+package config;
+
+public class Configuration {
+
+    public static String user="r.rengel99@gmail.com";
+    public static String password="renuc0pata";
+
+    public static String host="https://todo.ly";
+
+}
