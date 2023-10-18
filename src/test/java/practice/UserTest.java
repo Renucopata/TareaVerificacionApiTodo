0ccfb6967fdcef253ba3b9ffa147b531fdd8eba9
Rene Rengel <Rene.Rengel@jala.university>
2023-10-18 10:50:10 -0400

    TokenApi

---
+++ src/test/java/practice/UserTest.java
@@ -0,0 +1,32 @@
+package practice;
+
+import factoryRequest.FactoryRequest;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Test;
+import todoLyTest.TestBase;
+
+import static org.hamcrest.Matchers.equalTo;
+
+public class UserTest extends TestBase {
+
+    @Test
+    public void createUser(){
+        JSONObject body = new JSONObject();
+        body.put("Email", "userRenuco@user.com");
+        body.put("FullName", "usuario");
+        body.put("Password", "PassW0rd");
+
+        requestInfo.setUrl("https://todo.ly/api/user.json")
+                .setBody(body.toString());
+
+        response = FactoryRequest.make(post).send(requestInfo);
+
+        response.then().statusCode(200)
+                .body("FullName", equalTo(body.get("FullName")));
+
+        int idUser = response.then().extract().path("Id");
+        System.out.println("id es: " + idUser);
+    }
+
+
+}
